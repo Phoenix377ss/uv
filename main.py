d7cd0570e604b63c04ea373be85a517cab4145d0
phoenix377ss <phoenix377ss@gmail.com>
2025-12-11 09:00:07 +0700

    second part

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import pandas as pd
+import numpy as np
 def main():
     print("Hello from uv!")
-
+    print(f"Numpy version is {np.__version__}")
+    print(f"Pandas veriosn is {pd.__version__}")
 
 if __name__ == "__main__":
     main()
